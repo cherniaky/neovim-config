94e96396500de6eb6434c0ce9f43e762ca12b2b6
CherniakYura <juracherniak@gmail.com>
2024-02-12 15:41:56 +0100

    change theme to vscode

--- lua/yuriicherniak/packer.lua
+++ lua/yuriicherniak/packer.lua
@@ -13,13 +13,7 @@ return require('packer').startup(function(use)
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    })
+    use "Mofiqul/vscode.nvim"
 
     use {
         'nvim-treesitter/nvim-treesitter',
